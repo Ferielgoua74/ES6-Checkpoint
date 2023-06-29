2eec6a033e18503c0ed2bffd02c3e413f483134e
Ferielgoua74 <119040848+Ferielgoua74@users.noreply.github.com>
2023-06-29 12:24:01 +0100

    Create Find_the_capitals.js

---
+++ Find_the_capitals.js
@@ -0,0 +1,11 @@
+var capitals = function (word) {
+	let output = [];
+  for (let i=0; i<word.length; i++) {
+    if (word[i] == word[i].toUpperCase()) {
+      output.push(i);
+}
+}
+  return output;
+};
+
+console.log(capitals('CodEWaRs'));
